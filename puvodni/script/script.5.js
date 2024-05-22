c7852dcec9f9f976c5771e3732ffac361db98463
Michal Raus <raus.michal@email.cz>
2024-05-22 08:50:15 +0200

    Doplnění projektu Šifrovač, úprava názvu, úprava schopností, přidání async k funkcím, + PŘIDÁNÍ SLEDOVAČE

--- puvodni/script/script.5.js
+++ puvodni/script/script.5.js
@@ -1,22 +1,64 @@
 ﻿const v_port={id:["hd","c-svg"],
 idA:[["k1a","ao"],["k1b","ao"],["k1c","ao"],["k1d","ao"],["k1e","ao"],["k1f","ao"],["k2a","ao"],["k2b","ao"],["k2c","ao"],["k2d","ao"],["k2e","ao"],["k2f","ao"],["k3a","ao"],["k3b","ao"],["k3c","ao"],["k3d","ao"],["k3e","ao"],["k3f","ao"],["u1a","op"],["u1b","op"],["u1c","op"],["por1","ap"],["por2","ap"],["por3","ap"]],
 idO:[["kar1",0],["kar2",0],["kar3",0],["u1",0],["por",0]],
-a_p(id,t){
-/* funkce zapne animaci objektu ID po čase T */
-setTimeout(" document.getElementById('"+id+"').style.animationPlayState='running';",t);
-},
-handleEvent(){
-const vyska=window.visualViewport.height;
-const o1=document.getElementById(this.id[0]);
-const o2=document.getElementById(this.id[1]);
+pocet_pouziti:0, // určuje kolikrát uživatel použil visualViewport, který se spouští mimojiné scroolem uživatele
 
-o1.style.height=vyska+"px"; /* upraví výšku objektu podle VW port API */
-o2.style.height=vyska+"px";
-let d=this.idO.length;
+a_p(id,t){
+// funkce zapne animaci objektu ID po čase T
+setTimeout(`document.getElementById("${id}").style.animationPlayState="running";`,t); // spuštění animace za čas t
+},
+
+async statistika(){
+// funkce odešle připočtení statistiky po interakci stránky s uživatelem
+if(this.pocet_pouziti!=-1) // -1 této proměnné bude určovat blokaci v počítání, protože uživatel již byl připočítán
+{
+++this.pocet_pouziti; // přičte použití visualViewport
+if(this.pocet_pouziti>50) // pokud uživatel použil visualViewport více jak 50x
+{
+this.pocet_pouziti=-1; // nastavením proměnné na -1, určuje ukončení počítání použití visualViewport uživatelem
+
+const dataToSend='boar'; // data, která budou zaslána
+try{
+  // Vytvoření AJAX požadavku
+const xhr=new XMLHttpRequest();
+
+xhr.open('POST','statistika/zapis.php',true);
+xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+
+// Reakce na dokončení požadavku
+xhr.onload=()=>{
+if(xhr.status===200){
+// console.log('Data byla úspěšně odeslána.'); 
+}else{
+// console.log('Došlo k chybě při odesílání dat.');
+}
+};
+
+xhr.send('data='+encodeURIComponent(dataToSend));  // Odeslání dat
+}
+catch(err){
+console.log("Statistická data neodeslána. Chyba:"+err);
+}}}},
+
+handleEvent(){
+
+this.statistika(); // vede statistiku o návštěvnosti - odesláním dat
+
+const vyska=parseInt(window.visualViewport.height); // zjistí výšku visualViewport
+const page_Top=parseInt(window.visualViewport.pageTop); // zjistí horní polohu visualViewport
+const o1=document.getElementById(this.id[0]); // první objekt změny hlavička stránky
+const o2=document.getElementById(this.id[1]); // druhý objekt změny SVG obrázek
+
+o1.style.height=`${vyska}px`; // upraví výšku objektu podle visualViewport port API
+o2.style.height=`${vyska}px`; // upraví výšku objektu podle visualViewport port API
+
+let d=this.idO.length; // délka pole s animacemi
+
 for(let i=0;i<d;i++)
 {
-let ob=document.getElementById(this.idO[i][0]); /* objekt - kterým se spouští animace */
-if(((window.visualViewport.pageTop-(window.visualViewport.height/2)-(ob.offsetHeight/2)+200)>(ob.offsetTop))&&((window.visualViewport.pageTop-(window.visualViewport.height/2)-200-(ob.offsetHeight/2))<(ob.offsetTop)))
+let ob=document.getElementById(this.idO[i][0]); // objekt - kterým se spouští animace
+
+if(((page_Top-(vyska/2)-(ob.offsetHeight/2)+200)>(ob.offsetTop))&&((page_Top-(vyska/2)-200-(ob.offsetHeight/2))<(ob.offsetTop)))
 {
 /* podmínky a změna 3. parametru id objektu zabrání opětovnému spouštění animace */
 if(this.idO[i][1]==0)
@@ -103,19 +145,19 @@ window.visualViewport.addEventListener("scroll",this);
 addEventListener("scroll",this);
 },
 
-animaceAktivace(){
+async animaceAktivace(){
 /* přidělení CLASS s animací ID objektům */
-let id=this.idA.length;
+let id=this.idA.length; // délka pole animací3
 for(let i=0;i<id;i++)
 {
-let ob=document.getElementById(this.idA[i][0]);
-ob.className+=" "+this.idA[i][1];
+let ob=document.getElementById(this.idA[i][0]); // objekt HTML
+ob.className+=` ${this.idA[i][1]}`; // přidělení class objektu
 }},
 
 DEaktivace(){
-window.visualViewport.removeEventListener("resize",this);
-window.visualViewport.removeEventListener("scroll",this);
-removeEventListener("scroll",this);
+window.visualViewport.removeEventListener("resize",this); // posluchač na změnu velkosti okna stránky
+window.visualViewport.removeEventListener("scroll",this); // posluchač na scrool stránky
+removeEventListener("scroll",this); // posluchač na scrool stránky
 },
 
 zahajit(){
@@ -124,13 +166,15 @@ if(window&&window.visualViewport) /* test - zda je visualViewport podporováno *
 this.aktivace(); /* zapne posluchač visualViewport */
 this.animaceAktivace(); /* přepíše CLASS objektů na které se vztahuje animace */
 this.handleEvent(); /* aktivuje Handle, pokud by bylo najeto zrovna na objekt s animací - aby se spustila */
+setTimeout(this.handleEvent.bind(this),500); /* aktivuje Visual View port API + úprava hlavičky na 100vh - pro pomalejší zařízení za 500ms */
+setTimeout(this.handleEvent.bind(this),1000); /* aktivuje Visual View port API + úprava hlavičky na 100vh - pro ještě pomalejší zařízení za 1000ms */
 }}};
 
 v_port.zahajit(); /* aktivuje Visual View port API + úprava hlavičky na 100vh */
 
 
 const odkazy={t1:500,t2:1000,
-uprav(){
+async uprav(){
 let ob=document.querySelectorAll("a"); /* najde včechny tagy A na stránce a udělá z nich pole */
 let ob_s=ob.length;
 for(let i=0;i<ob_s;i++)
@@ -139,15 +183,15 @@ let hr=ob[i].href; /* načte href objektu */
 let hr_p=hr.indexOf("#"); /* pozice # v řetězci */
 if(hr_p!=-1) /* pokud se pozice v řetězci == -1 , tak nebyl znak v řetězci nalezen */
 {
-let poz_rez=hr_p + 1; /* posune polohu řezu pro odkaz o jedno místo od # */
+let poz_rez=hr_p+1; /* posune polohu řezu pro odkaz o jedno místo od # */
 let odkaz=ob[i].href.slice(poz_rez); /* vytvoří konečný odkaz ořezáním původního */
 if(odkaz!="") /* pokud nebude odkaz prázdným řetězcem */
 {
-ob[i].href="javascript:odkazy.roluj('"+odkaz+"');"; /* upravý href každého odkazu na javascriptovou funkci */
+ob[i].href=`javascript:odkazy.roluj('${odkaz}');`; /* upravý href každého odkazu na javascriptovou funkci */
 
 if(odkaz=="boar-cz")
 {
-ob[i].href="javascript:odkazy.roluj('"+odkaz+"',1);"; /* výjimka pro tlačíto Boar-cz, které dělá krátký posun z menu do první části Article */
+ob[i].href=`javascript:odkazy.roluj('${odkaz}',1);`; /* výjimka pro tlačíto Boar-cz, které dělá krátký posun z menu do první části Article */
 }
 }}}},
 
@@ -157,12 +201,12 @@ if(ne==0) /* pokud není druhý parametr funkce roven nule - neprovede blokaci a
 v_port.DEaktivace(); /* vypne posluchač pro animace */
 }
 document.getElementById(id).scrollIntoView({behavior:"smooth",block:"start"}); /* provede scrool na objekt */
-setTimeout("document.getElementById('"+id+"').scrollIntoView({behavior:'smooth',block:'start'});",this.t1); /* za 500ms provede opět scrool na objekt */
-setTimeout("v_port.aktivace();v_port.handleEvent();",this.t2); /* za 1s opět zapne posluchač pro animace */
+setTimeout(`document.getElementById('${id}').scrollIntoView({behavior:'smooth',block:'start'});`,this.t1); /* za 500ms provede opět scrool na objekt */
+setTimeout(`v_port.aktivace();v_port.handleEvent();`,this.t2); /* za 1s opět zapne posluchač pro animace */
 }};
 
-const sdilet={_idFB:"sdil-fb",_idTW:"sdil-tw",_hrefFB:null,_hrefTW:null,SIRKA:600,VYSKA:600,min_VYSKA:800,min_SIRKA:800,
-prepis(){
+const sdilet={_idFB:"sdil-fb",_idTW:"sdil-tw",SIRKA:600,VYSKA:600,min_VYSKA:800,min_SIRKA:800,
+async prepis(){
 let vyska=parseInt(window.screen.height); /* výška obrazovky */
 let sirka=parseInt(window.screen.width); /* šířka obrazovky */
 let z_leva=sirka/2-this.SIRKA/2;
@@ -171,20 +215,19 @@ let z_hora=vyska/2-this.VYSKA/2;
 /* funkce přepíše HREF na tlačítkách sdílet Facebook a sdílet Twitter */
 if(document.getElementById(this._idFB)&&vyska>this.min_VYSKA&&sirka>this.min_SIRKA)
 {
-this._hrefFB=document.getElementById(this._idFB).href;
-document.getElementById(this._idFB).target=""; /* target musí být prázdý jinak nové okno neotevře */
-let textFB="window.open('"+this._hrefFB+"','Sdílet na FB','width="+this.SIRKA+",height="+this.VYSKA+",left="+z_leva+",top="+z_hora+"');";
-document.getElementById(this._idFB).href="javascript:"+textFB;
+const hrefFB=document.getElementById(this._idFB).href; // načte stávající href odkazu
+document.getElementById(this._idFB).target=""; // target musí být prázdý jinak nové okno neotevře
+let textFB=`window.open('${hrefFB}','Sdílet na FB','width=${this.SIRKA},height=${this.VYSKA},left=${z_leva},top=${z_hora}');`; // příprava nového href
+document.getElementById(this._idFB).href=`javascript:${textFB}`; // dokončení nového href
 }
 if(document.getElementById(this._idTW)&&vyska>this.min_VYSKA&&sirka>this.min_SIRKA)
 {
-this._hrefTW=document.getElementById(this._idTW).href;
+const hrefTW=document.getElementById(this._idTW).href;
 document.getElementById(this._idTW).target=""; /* target musí být prázdý jinak nové okno neotevře */
-let textTW="window.open('"+this._hrefTW+"','Sdílet na Twittru','width="+this.SIRKA+",height="+this.VYSKA+",left="+z_leva+",top="+z_hora+"');";
-document.getElementById(this._idTW).href="javascript:"+textTW;
+let textTW=`window.open('${hrefTW}','Sdílet na Twittru','width=${this.SIRKA},height=${this.VYSKA},left=${z_leva},top=${z_hora}');`;
+document.getElementById(this._idTW).href=`javascript:${textTW}`;
 }}};
 
-setTimeout("v_port.zahajit();",500); /* aktivuje Visual View port API + úprava hlavičky na 100vh - pro pomalejší zařízení za 500ms */
-setTimeout("v_port.zahajit();",1000); /* aktivuje Visual View port API + úprava hlavičky na 100vh - pro ještě pomalejší zařízení za 1000ms */
+
 odkazy.uprav(); /* opraví odkazi na stránce na SCROOL */
 sdilet.prepis(); /* zajistí přepis HREF tlačítek pro sdílení na Facebooku a Twittru */
